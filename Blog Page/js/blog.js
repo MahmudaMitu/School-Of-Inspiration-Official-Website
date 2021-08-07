c51107a19983e230cfa163c501537139b81f7966
Amanda Reahana Gomes <reahana233@gmail.com>
2021-08-08 00:33:16 +0600

    for navbar scrolling

---
+++ Blog Page/js/blog.js
@@ -0,0 +1,17 @@
+
+window.onscroll = function() {scrollFunction()};
+        
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "15px 10px";
+    document.getElementById("navbar").style.backgroundColor = "#161e29";
+    document.getElementById("navbar").style.marginTop = "-5px";
+    document.getElementById("logo").style.fontSize = "20px";     
+    document.getElementById("navbar").style.boxShadow = "0 0 5px 5px #fff";      
+    
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("links").style.marginTop = "100px"; 
+  }
+}
