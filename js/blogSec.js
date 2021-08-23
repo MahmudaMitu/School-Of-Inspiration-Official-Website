2764703d3ae882c8341ecdfe0f2d4ad2bda732a2
Amanda Reahana Gomes <reahana233@gmail.com>
2021-08-23 19:59:37 +0600

    function for blogsection

--- js/blogSec.js
+++ js/blogSec.js
@@ -1,57 +1,30 @@
-$(document).ready(function(){
-    var typed = new Typed(".typing", {
-      strings: [ "Read Our Blogs....","About our workshops....", "Our Trainings....","Meet Campus Leaders...."],
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true
-    });
+$(document).ready(function () {
+  var typed = new Typed(".typing", {
+    strings: ["Read Our Blogs....", "About Latest News....", "A Lots More...."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
   // owl carousel script
-$('.carousel').owlCarousel({
+  $('.carousel').owlCarousel({
     margin: 20,
     loop: true,
     autoplayTimeOut: 2000,
     autoplayHoverPause: true,
     responsive: {
-        0:{
-            items: 1,
-            nav: false
-        },
-        600:{
-            items: 2,
-            nav: false
-        },
-        1000:{
-            items: 3,
-            nav: false
-        }
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
     }
-});
-// <script>
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-// </script>
   });
+
+});
